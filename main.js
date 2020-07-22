777f767db54b36f27e36ce6e3ae756f9fb8fe7d7
dhakal101 <50218546+gdhakal123@users.noreply.github.com>
2020-07-22 11:37:15 -0600

    s

--- main.js
+++ main.js
@@ -3,18 +3,20 @@
 // Set up Canvas and Graphics Context
 let cnv = document.getElementById("myCanvas");
 let ctx = cnv.getContext("2d");
-cnv.width = 800;
-cnv.height = 600;
+initGameLibrary(800, 600);
 
 // Main Program Loop
 requestAnimationFrame(draw);
 
 function draw() {
-    // Logic
+  // Logic
+  console.log(keyIsPressed);
 
-    // Drawing
-    ctx.clearRect(0, 0, cnv.width, cnv.height);
+  // Drawing
+  ctx.clearRect(0, 0, cnv.width, cnv.height);
 
-    // Request another Animation Frame
-    requestAnimationFrame(draw);
+  ctx.fillRect(mouseX, mouseY, 50, 50);
+
+  // Request another Animation Frame
+  requestAnimationFrame(draw);
 }
